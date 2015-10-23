d2e2538e4325db9fe1404752d0084254cb3e352c
Fernando <Fernandobarbosaneto@hotmail.com>
2015-10-23 16:17:36 -0200

    pequeno passo para a humanidade

---
+++ Genero.java
@@ -0,0 +1,20 @@
+
+public class Genero {
+
+	private String sigla;
+	private String nome;
+	
+	public Genero(String sigla, String nome) {
+		this.sigla = sigla;
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+}
---
+++ Pessoa.java
@@ -0,0 +1,21 @@
+
+public class Pessoa {
+	
+	private int codigo;
+	private String nome;
+
+	public Pessoa(int codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+}
